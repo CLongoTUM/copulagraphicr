a4ff8c806e02d6db58b8ff628038d070c2ed0ab1
Claudio Longo <ClaudioLongo2212@gmail.com>
2023-01-13 16:31:44 +0100

    Added documentation for the function load_data. Added "Melanoma" dataset to inst.

--- R/load_data.R
+++ R/load_data.R
@@ -1,21 +1,26 @@
-library("riskRegression")
-library("dplyr")
-library("lubridate")
-library("magrittr")
-load_data <- function() {
-
-  load(file = paste0("C:/Users/", Sys.getenv("USERNAME"), "/OneDrive/Documents/R (git)/Projects/projectr/inst/Melanoma.rda"))
-  # system.file("Melanoma.rda")
+#' The "load_data" function loads data from data_path and extracts
+#' the competing risk data consisting of observations (T_i, delta_i).
+#'
+#' @param data_path Path to file. Files with extension '.csv' are supported.
+#'
+#' @export
+#'
+#' @examples # load data set:
+#' df <- load_data()
+load_data <- function(data_path = system.file("inst/Melanoma.csv", package = "copulagraphicr")) {
 
   ## competing risk data
-  df <- Melanoma
+  # variables: time; delta
 
-  # time: time in days from operation, stopped after "event" occurred
-  # status: 1 = died from melanoma; 0 = censored
+  ## variable description
+  # time: time in days from operation, stopped after "risk-event" occurred
+  # risk-events for example data: X (time to death), Y (time to censoring)
+  # delta: indicator, in risk-event was observed or censored, 1 if X =< Y; 0 if X > Y
 
-  # risk: X (time to death), Y (time to censoring)
-  # observations (T_i, delta_i), delta = 1 if X =< Y
-  sample_data <- data.table::data.table(T = df$time, delta = df$status)
+  # observations (T_i, delta_i)
+  sample_data <- data.table::fread(data_path) %>%
+    select("time", "delta")
 }
+
 
 
