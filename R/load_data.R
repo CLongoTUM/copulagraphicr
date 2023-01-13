8f246ac1838971b84acabd3de8820f7f90957330
Claudio Longo <ClaudioLongo2212@gmail.com>
2023-01-13 17:51:56 +0100

    Added documentation for load_data function. Deleted redundant dataset. Added LICENSE.

--- R/load_data.R
+++ R/load_data.R
@@ -1,25 +1,33 @@
-#' The "load_data" function loads data from data_path and extracts
-#' the competing risk data consisting of observations (T_i, delta_i).
+#' Data Input: load competing risk data
+#'
+#' The `load_data` function loads data from `data_path` and extracts
+#' the competing risk data consisting of observations (T\eqn{_i}, \eqn{\delta_i}).
+#'
+#' The competing risk data should consist of the observed sample data
+#' (T\eqn{_i}, \eqn{\delta_i}).
+#' \itemize{
+#' \item `competing risks`:
+#' \itemize{
+#' \item \eqn{X} (time to death)
+#' \item \eqn{Y} (censoring time)
+#' }
+#' \item `sample data`:
+#' \itemize{
+#' \item `time to event` \eqn{T =} min(X,Y)
+#' \item `event-indicator` \eqn{\delta} = 1 if \eqn{X \le Y}; \eqn{\delta} = 0 if \eqn{X > Y}
+#' }
+#' }
 #'
 #' @param data_path Path to file. Files with extension '.csv' are supported.
 #'
+#' @importFrom magrittr "%>%"
 #' @export
 #'
-#' @examples # load data set:
-#' df <- load_data()
+#' @examples # load data set: df <- load_data()
 load_data <- function(data_path = system.file("inst/Melanoma.csv", package = "copulagraphicr")) {
-
-  ## competing risk data
-  # variables: time; delta
-
-  ## variable description
-  # time: time in days from operation, stopped after "risk-event" occurred
-  # risk-events for example data: X (time to death), Y (time to censoring)
-  # delta: indicator, in risk-event was observed or censored, 1 if X =< Y; 0 if X > Y
-
-  # observations (T_i, delta_i)
   sample_data <- data.table::fread(data_path) %>%
-    select("time", "delta")
+    dplyr::select("time", "delta")
+  return(sample_data)
 }
 
 
